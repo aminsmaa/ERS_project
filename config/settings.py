3f778a1b19f7f4dbbf0059d03e07bee43c9a2ce9
Ferdows-PC <aminsmaa77@gmail.com>
2023-08-09 00:36:30 +0330

    update

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q58fpl)8*d!&s^@slk_0c)74@u@d759y!m2wc*&^uu4l%(@9)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
